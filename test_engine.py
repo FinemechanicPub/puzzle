a6b477edae73efe0b8e933654d4d61a9a9f5272a
Alexander Rubtsov <averoesmsk@gmail.com>
2024-06-05 14:39:48 +0300

    More tests

--- test_engine.py
+++ test_engine.py
@@ -13,24 +13,48 @@ def visualize(board: Board, mask, empty=" ", fill="*"):
     return [string[row*width:(row+1)*width] for row in range(height)]
 
 
-if __name__ == "__main__":
-    board = Board(15, 4)
-    # piece_masks = {
-    #     name: board.piece_masks(points) for name, points in pieces.items()
-    # }
-    # for mask in piece_masks["L"]:
-    #     s = visualize(board, mask)
-    #     print("".join(["="] * board.width))
-    #     print(*s, sep="\n")
-    #     print("".join(["="] * board.width))
-    #     print("")
+def print_board(board: Board, mask):
+    s = visualize(board, mask)
+    print("".join(["="] * board.width))
+    print(*s, sep="\n")
+    print("".join(["="] * board.width))
+    print("")
 
+
+if __name__ == "__main__":
+    # board = Board(15, 4)
+    # # piece_masks = {
+    # #     name: board.piece_masks(points) for name, points in pieces.items()
+    # # }
+    # # for mask in piece_masks["L"]:
+    # #     s = visualize(board, mask)
+    # #     print("".join(["="] * board.width))
+    # #     print(*s, sep="\n")
+    # #     print("".join(["="] * board.width))
+    # #     print("")
+
+    # piece_set = tuple(
+    #     invert(tuple(
+    #         board.piece_masks(rotation)
+    #         for rotation in produce_rotations(piece)
+    #     ))
+    #     for piece in pieces.values()
+    # )
+    # s = list(solutions(board, piece_set))
+    # print(f"Solutions found: {len(s)}")
+
+    board = Board(12, 5)
+    placed_pieces = int("111111111111111111111111111111111111100111101110011111111111", 2)
+    print(placed_pieces.bit_length())
+    print_board(board, placed_pieces)
+    available_pieces = {"U": pieces["U"]}
+    print(available_pieces)
     piece_set = tuple(
         invert(tuple(
             board.piece_masks(rotation)
             for rotation in produce_rotations(piece)
         ))
-        for piece in pieces.values()
+        for piece in available_pieces.values()
     )
-    s = list(solutions(board, piece_set))
-    print(f"Solutions found: {len(s)}")
+    solution = next(solutions(board, piece_set, placed_pieces))
+    print(solution)
