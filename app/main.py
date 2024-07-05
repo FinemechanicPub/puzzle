7a413fae8062ddf7df8c460e79be5739f246d03e
Alexander Rubtsov <averoesmsk@gmail.com>
2024-07-05 21:53:21 +0300

    Improve operation ids for OpenAPI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.routing import APIRoute
 
 from app.core.config import settings
 from app.api.routers import main_router
 
+
+# Настройка идентификаторов для OpenAPI
+def custom_generate_unique_id(route: APIRoute):
+    return f"{route.tags[0]}-{route.name}"
+
+
 # Создание объекта приложения.
-app = FastAPI(title=settings.app_title)
+app = FastAPI(
+    title=settings.app_title,
+    generate_unique_id_function=custom_generate_unique_id,
+)
 
 # Подключение роутера
 app.include_router(main_router)
