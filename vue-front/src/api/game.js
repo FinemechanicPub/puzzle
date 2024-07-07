e7f4dac6fb3a2279d383b8566fb3116f784703fd
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-08 01:26:46 +0300

    Fix content-type header

--- vue-front/src/api/game.js
+++ vue-front/src/api/game.js
@@ -1,6 +1,6 @@
 async function getGame(gameId){
     var uri = `${import.meta.env.VITE_API_BASE_URI}/games/${gameId}/full/`
-    var response = await fetch(uri)
+    var response = await fetch(uri, { headers: { "Content-Type": "application/json; charset=utf-8" }})
     var game = await response.json()
     return game
 }
--- vue-front/src/api/hint.js
+++ vue-front/src/api/hint.js
@@ -2,7 +2,7 @@ async function getHint(gameId, installedPieces){
     var uri = `${import.meta.env.VITE_API_BASE_URI}/play/hint/`
     const requestOptions = {
         method: "PUT",
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json; charset=utf-8" },
         body: JSON.stringify(
             {
                 game_id: gameId,
