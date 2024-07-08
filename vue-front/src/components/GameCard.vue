ab1169e90a7d2fc0bc6e5265cce2b93a3161d330
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-09 02:10:33 +0300

    Add main page draft design

---
+++ vue-front/src/components/GameCard.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps({
+        game: Object
+    })
+</script>
+
+<style scoped>
+.rounded-box{
+    border-radius: 10px;
+    padding: 20px;
+    margin: 1ch auto;
+    width: -moz-fit-content;
+    width: fit-content;
+    font-weight: bolder;
+    color: slateblue;
+    background: mediumaquamarine;
+    border: 2px solid lightseagreen;
+    text-align: center; 
+}
+
+</style>
+
+<template>
+    <div class="rounded-box">
+        {{ props.game.title }}
+    </div>
+</template>
--- vue-front/src/views/MainPage.vue
+++ vue-front/src/views/MainPage.vue
@@ -1,7 +1,55 @@
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import getGames from '@/api/cover.js'
+    import GameCard from '@/components/GameCard.vue';
+
+    const error = ref(null)
+    const loading = ref(false)
+    const games = ref([])
+
+    onMounted(fetchData)
+    
+
+    async function fetchData() {
+        error.value = games.value = null
+        loading.value = true
+        
+        try {
+            console.log("start fetch")
+            games.value = await getGames()
+            console.log("start fetch complete")
+            console.log(games.value)
+        } catch (err) {
+            console.log("fetch error")
+            if (err instanceof TypeError){
+                error.value = err.message
+            } else {
+                error.value = err.toString()
+            }
+        } finally {
+            loading.value = false
+        }
+    }
+</script>
+
+<style scoped>
+.game-roulette{
+    display: flex;
+    flex-direction: row;
+    gap: 5%;
+    justify-content: center;
+    align-content: baseline;
+    margin: 5ch auto;
+}
+
+</style>
+
 <template>
-    <div>
-        <RouterLink :to="{name: 'game', params: {id: 17}}">
-            Запуск
-        </RouterLink>
+    <div v-if="games" class="game-roulette">
+        <div :key="game.id" v-for="game in games">
+            <RouterLink :to="{name: 'game', params: {id: game.id}}">
+                <GameCard :game="game" />
+            </RouterLink>
+        </div>
     </div>
 </template>
