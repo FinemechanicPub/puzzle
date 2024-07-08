1d417dd2ada4823332a48553bb41f865859c55c6
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-08 19:57:26 +0300

    Add settings

--- app/core/config.py
+++ app/core/config.py
@@ -8,6 +8,8 @@ class Settings(BaseSettings):
     connection_string: str = 'sqlite+aiosqlite://'
     max_cells: int = 120
     page_limit: int = 20
+    max_page: int = 100
+    default_color: int = 14531481
 
 
 settings = Settings()
