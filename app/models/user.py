afeca0a5d403000e1242db8ff4917f820ffe399c
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-09 04:08:56 +0300

    Add access token table

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,21 @@
 from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users_db_sqlalchemy.access_token import (
+    SQLAlchemyBaseAccessTokenTable,
+)
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import declared_attr, Mapped, mapped_column
+from sqlalchemy.types import Integer
 
-from app.models.base import Base
+from app.models.base import Base, EmptyBase
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
     pass
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTable[int], EmptyBase):
+    @declared_attr
+    def user_id(cls) -> Mapped[int]:
+        return mapped_column(
+            Integer, ForeignKey('user.id', ondelete="cascade"), nullable=False
+        )
