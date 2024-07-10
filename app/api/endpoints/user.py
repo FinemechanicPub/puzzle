287340421db972261c8c6d62eface01bf28a7ffd
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-11 00:06:44 +0300

    Add custom routers for authentication

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,13 +1,48 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi_users.authentication import Strategy
 
-from app.core.user import fastapi_users, auth_backend
+from app.api.exceptions import BadCredentialsException, UserNotVerifiedException
+from app.core.config import settings
+from app.core.user import UserManager, auth_backend, get_user_manager, user_token
+from app.schemas.user import LoginRequest
 
 user_router = APIRouter()
 
 
-# https://fastapi-users.github.io/fastapi-users/latest/configuration/routers/auth/
-user_router.include_router(
-    fastapi_users.get_auth_router(auth_backend, requires_verification=True),
-    prefix="/auth",
-    tags=["auth"],
-)
+# # https://fastapi-users.github.io/fastapi-users/latest/configuration/routers/auth/
+# user_router.include_router(
+#     fastapi_users.get_auth_router(auth_backend, requires_verification=True),
+#     prefix="/auth",
+#     tags=["auth"],
+# )
+
+
+@user_router.post('/login')
+async def login(
+    login_details: LoginRequest,
+    request: Request,
+    user_manager: UserManager = Depends(get_user_manager),
+    strategy: Strategy = Depends(auth_backend.get_strategy),
+):
+    user = await user_manager.authenticate(
+        OAuth2PasswordRequestForm(
+            username=login_details.email, password=login_details.password
+        )
+    )
+    if user is None or not user.is_active:
+        raise BadCredentialsException
+    if not settings.allow_unverified_users and not user.is_verified:
+        raise UserNotVerifiedException
+    response = await auth_backend.login(strategy, user)
+    await user_manager.on_after_login(user, request, response)
+    return response
+
+
+@user_router.post('/logout',)
+async def logout(
+    user_token: tuple = Depends(user_token),
+    strategy: Strategy = Depends(auth_backend.get_strategy),
+):
+    user, token = user_token
+    return await auth_backend.logout(strategy, user, token)
--- app/api/routers.py
+++ app/api/routers.py
@@ -5,10 +5,17 @@ from app.api.endpoints.game import game_router
 from app.api.endpoints.piece import piece_router
 from app.api.endpoints.play import play_router
 from app.api.endpoints.user import user_router
+from app.core.user import fastapi_users
+from app.schemas.user import UserRead, UserUpdate
 
 main_router = APIRouter()
 main_router.include_router(cover_router, prefix='/cover', tags=['Main Page'])
 main_router.include_router(game_router, prefix='/games', tags=['Games'])
 main_router.include_router(piece_router, prefix='/pieces', tags=['Pieces'])
 main_router.include_router(play_router, prefix='/play', tags=['Games'])
-main_router.include_router(user_router, prefix='/user')
+main_router.include_router(user_router, prefix='/auth', tags=['Authentication'])
+
+main_router.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users", tags=["Users"],
+)
