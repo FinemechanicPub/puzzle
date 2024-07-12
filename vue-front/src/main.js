212dcd2324f5ee26310aa7a5280e789db4e96668
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-12 03:38:19 +0300

    Setup base API url

--- vue-front/src/main.js
+++ vue-front/src/main.js
@@ -3,7 +3,9 @@ import './assets/puzzle.css'
 
 import { createApp } from 'vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import { OpenAPI } from  './api/generated/core/OpenAPI';
 import App from './App.vue'
 import router from './routers/router'
 
+OpenAPI.BASE = `${import.meta.env.VITE_API_BASE_URI}`
 createApp(App).use(autoAnimatePlugin).use(router).mount('#app')
