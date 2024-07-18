19cf6ffb5987de0ad25b2dde8c0a1fc7ba742cac
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-19 02:57:32 +0300

    Add game thumbnail endpoint

--- backend/app/api/endpoints/game.py
+++ backend/app/api/endpoints/game.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, status
+from io import BytesIO
+
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.api.exceptions import GameNotFoundException
@@ -9,6 +11,9 @@ from app.repositories.game_repository import game_extended_repository, game_repo
 from app.schemas.game import (
     CreateGameRequest, GameResponseBase, GameLongResponse
 )
+from app.services.image import thumbnail
+from engine.board import Board
+from engine.piece import Piece
 
 
 game_router = APIRouter()
@@ -64,3 +69,30 @@ async def remove_game(
         raise GameNotFoundException
     await game_extended_repository.remove(session, game)
     return
+
+
+@game_router.get(
+    '/{game_id}/thumbnail/',
+    responses={
+        200: {"content": {"image/png": {}}},
+        404: {"description": "Game not found"},
+    },
+    response_class=Response,
+)
+async def game_thumbnail(
+    game_id: int,
+    session: AsyncSession = Depends(get_async_session)
+):
+    game = await game_extended_repository.get(session, game_id)
+    if not game:
+        raise GameNotFoundException
+    image = thumbnail(
+        Board(game.height, game.width),
+        (
+            Piece(game_piece.points, game_piece.color or game_piece.default_color)
+            for game_piece in game.game_pieces
+        )
+    )
+    with BytesIO() as data:
+        image.save(data, "png")
+        return Response(content=data.getvalue(), media_type="image/png")
