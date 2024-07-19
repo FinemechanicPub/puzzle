4fdecfc1a2a350d5d30b4bb06e9705c6169ead3e
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-19 16:18:44 +0300

    Use short response form for cover page

--- backend/app/api/endpoints/cover.py
+++ backend/app/api/endpoints/cover.py
@@ -2,13 +2,13 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import APIRouter, Depends
 
 from app.core.db import get_async_session
-from app.repositories.game_repository import game_extended_repository
-from app.schemas.game import GameShortResponse
+from app.repositories.game_repository import game_repository
+from app.schemas.game import GameResponseBase
 
 cover_router = APIRouter()
 
 
-@cover_router.get('/suggestion/', response_model=list[GameShortResponse])
+@cover_router.get('/suggestion/', response_model=list[GameResponseBase])
 async def suggestion(session: AsyncSession = Depends(get_async_session)):
-    games = await game_extended_repository.list(session, offset=0, limit=3)
+    games = await game_repository.list(session, offset=0, limit=3)
     return games
