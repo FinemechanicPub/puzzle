ae7b79299eb5d6ce0e6599eada9a53c4d25e5a1e
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-23 05:23:19 +0300

    Suggest random games

--- backend/app/api/endpoints/cover.py
+++ backend/app/api/endpoints/cover.py
@@ -10,5 +10,5 @@ cover_router = APIRouter()
 
 @cover_router.get('/suggestion/', response_model=list[GameResponseBase])
 async def suggestion(session: AsyncSession = Depends(get_async_session)):
-    games = await game_repository.list(session, offset=0, limit=3)
+    games = await game_repository.list(session, offset=10, limit=10, random=True)
     return games
