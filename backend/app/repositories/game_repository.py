772c2100b290c0bfaebf2896b28bc74385876526
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-07-25 01:09:04 +0300

    Add repository with piece_count

--- backend/app/repositories/game_repository.py
+++ backend/app/repositories/game_repository.py
@@ -1,7 +1,20 @@
 from sqlalchemy.orm import joinedload
+from sqlalchemy.sql.expression import func
 
 from app.models.game import Game, GamePieces, Piece
 from .repository import RepositoryBase
+
+piece_count = func.count(GamePieces.game_id).label("piece_count")
+
+
+class GameCountRepository(RepositoryBase[Game]):
+
+    def selector(self, piece_count):
+        return (
+            super().selector().join(GamePieces)
+            .group_by(self.model.id)
+            .having(func.count(GamePieces.game_id) == piece_count)
+        )
 
 
 class GameExtendedRepository(RepositoryBase[Game]):
@@ -17,4 +30,5 @@ class GameExtendedRepository(RepositoryBase[Game]):
 
 
 game_repository = RepositoryBase(Game)
+game_count_repository = GameCountRepository(Game)
 game_extended_repository = GameExtendedRepository(Game)
