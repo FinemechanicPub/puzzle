cbc39406ebcfc77eb52bb4f4b458f8e16dab9d2b
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-05 16:09:20 +0300

    Fix SQLite detection issue

--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,8 +1,10 @@
+import logging
 from sqlalchemy import event
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlite3 import Connection as SQLite3Connection
 
 from app.core.config import settings
+
+logger = logging.getLogger(__name__)
 
 engine = create_async_engine(settings.connection_string)
 AsyncSessionLocal = async_sessionmaker(engine)
@@ -10,7 +12,9 @@ AsyncSessionLocal = async_sessionmaker(engine)
 
 @event.listens_for(engine.sync_engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
-    if isinstance(dbapi_connection, SQLite3Connection):
+    # SQLAlchemy doesn't provide better means to check against database engine
+    if hasattr(dbapi_connection.dbapi, "sqlite"):
+        logger.debug("Enforcing foreign keys in SQLite")
         cursor = dbapi_connection.cursor()
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
