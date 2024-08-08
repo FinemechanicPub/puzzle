7de5740c4657cc33640fd1a9a97927f8555beaca
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-08 03:24:58 +0300

    Implement disjoint set data structure

---
+++ backend/engine/disjointset.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+
+class DisjointSet:
+    def __init__(self, size: int):
+        self.parent = [i for i in range(size)]
+        self.rank = [0] * size
+
+    def find(self, item: int):
+        if item == self.parent[item]:
+            return item
+        self.parent[item] = self.find(self.parent[item])
+        return self.parent[item]
+
+    def union(self, first: int, second: int):
+        first = self.find(first)
+        second = self.find(second)
+        if first == second:
+            return
+        if self.rank[first] < self.rank[second]:
+            first, second = second, first
+        self.parent[second] = first
+        if self.rank[first] == self.rank[second]:
+            self.rank[first] += 1
+
+    def sets(self):
+        _sets: dict[int, int] = defaultdict(int)
+        for item in range(len(self.parent)):
+            _sets[self.find(item)] += 1
+        return list(_sets.values())
