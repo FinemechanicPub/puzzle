073d16294c25ca64684f8daf18c27d5110f0fb6e
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-13 21:57:46 +0300

    Add not found page

--- frontend/src/routers/router.js
+++ frontend/src/routers/router.js
@@ -3,10 +3,12 @@ import { createWebHistory, createRouter } from 'vue-router'
 // https://router.vuejs.org/guide/advanced/lazy-loading.html
 const MainPage = () => import('@/views/MainPage.vue')
 const GameView = () => import('@/views/GameView.vue')
+const NotFound = () => import('@/views/NotFound.vue')
 
 const routes = [
   { path: '/', name: 'main', component: MainPage},
   { path: '/game/:id', name: 'game', component: GameView, props: true},
+  { path: "/:notFound", component: NotFound},
 ]
 
 const router = createRouter({
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,50 @@
+<script setup>
+</script>
+<style scoped>
+  .mainbox {
+    background-color: white;
+    margin: auto;
+    height: 600px;
+    width: 600px;
+    position: relative;
+  }
+  .err {
+    color: green;
+    font-family: sans-serif;
+    font-size: 11rem;
+    position:absolute;
+    left: 30%;
+    top: 0%;
+  }
+  .msg {
+    text-align: center;
+    font-family: sans-serif;
+    font-size: 1.6rem;
+    position:absolute;
+    left: 16%;
+    top: 45%;
+    width: 75%;
+  }
+  a {
+    text-decoration: none;
+    color: blue;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
+<template>
+  <div class="mainbox">
+    <div class="err">404</div>    
+    <div class="msg">
+      Страница не найдена
+      <p>
+        Возможно, тут что-то было или появится в будущем, но сейчас здесь ничего нет.
+      </p>
+      <p>
+        Попробуйте главную 
+        <RouterLink to="/">страницу</RouterLink>.
+      </p>
+    </div>
+  </div>
+</template>
