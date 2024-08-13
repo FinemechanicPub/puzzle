5594f4ff44b2c0f4fbe8c8f5d40120af9479f2cb
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-13 04:02:20 +0300

    Hide scroll buttons when there is just one card

--- frontend/src/components/GameCarousel.vue
+++ frontend/src/components/GameCarousel.vue
@@ -148,7 +148,7 @@
                 </button>
             </div>
         </div>
-        <button class="carousel-button prev" @click="prevCard" :disabled="currentIndex === 0">&lt;</button>
-        <button class="carousel-button next" @click="nextCard" :disabled="currentIndex === games.length - 1">&gt;</button>
+        <button v-if="games.length > 1" class="carousel-button prev" @click="prevCard" :disabled="currentIndex === 0">&lt;</button>
+        <button v-if="games.length > 1" class="carousel-button next" @click="nextCard" :disabled="currentIndex === games.length - 1">&gt;</button>
     </div>
 </template>
