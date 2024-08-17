bb746b9415473c18c781072095bb415ad587815e
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-17 18:08:19 +0300

    Attach library

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,11 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import { OpenAPI } from  './api/generated/core/OpenAPI';
-import App from './App.vue'
-import router from './routers/router'
+import Vue3Tour from 'vue3-tour';
+
+import App from './App.vue';
+import router from './routers/router';
 
 if (import.meta.env.DEV){
     console.log(`base api url: ${import.meta.env.VITE_API_BASE_URI}`)
     OpenAPI.BASE = `${import.meta.env.VITE_API_BASE_URI}`
 }
-createApp(App).use(autoAnimatePlugin).use(router).mount('#app')
+createApp(App).use(autoAnimatePlugin).use(router).use(Vue3Tour).mount('#app')
