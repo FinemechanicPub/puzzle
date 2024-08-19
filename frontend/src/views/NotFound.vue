896696c5169f8823c94e2e6a4a9dcdb16a5c1025
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-19 17:18:14 +0300

    Fix styles

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -2,42 +2,26 @@
 </script>
 <style scoped>
   .mainbox {
-    background-color: white;
-    margin: auto;
-    height: 600px;
-    width: 600px;
-    position: relative;
+    background-color: var(--color-background);
+    text-align: center;
+    margin: auto 5%;
   }
   .err {
-    color: green;
+    color: var(--color-text);
     font-family: sans-serif;
     font-size: 11rem;
-    position:absolute;
-    left: 30%;
-    top: 0%;
   }
   .msg {
     text-align: center;
     font-family: sans-serif;
     font-size: 1.6rem;
-    position:absolute;
-    left: 16%;
-    top: 45%;
-    width: 75%;
-  }
-  a {
-    text-decoration: none;
-    color: blue;
-  }
-  a:hover {
-    text-decoration: underline;
   }
 </style>
 <template>
   <div class="mainbox">
     <div class="err">404</div>    
     <div class="msg">
-      Страница не найдена
+      Страница не найдена.
       <p>
         Возможно, тут что-то было или появится в будущем, но сейчас здесь ничего нет.
       </p>
