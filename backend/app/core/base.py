21b9e4d83218c7a250a5523a606c6b3bd7dec698
FinemechanicPub <93194456+FinemechanicPub@users.noreply.github.com>
2024-08-24 20:26:55 +0300

    Add menu model into list for alembic

--- backend/app/core/base.py
+++ backend/app/core/base.py
@@ -2,4 +2,5 @@
 
 from app.models.base import Base  # noqa
 from app.models.game import Game, GamePieces, Piece, PieceRotation  # noqa
+from app.models.menu import Menu  # noqa
 from app.models.user import User  # noqa
